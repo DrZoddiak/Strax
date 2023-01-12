04114926fbe53ec5b93c66d42da581581fbef0bc
Jesse <jessemckee97@gmail.com>
2023-01-11 19:52:51 -0600

    Add overwrite flag for commands

--- src/main/kotlin/me/zodd/strax/core/commands/CommonCommandFlags.kt
+++ src/main/kotlin/me/zodd/strax/core/commands/CommonCommandFlags.kt
@@ -1,12 +1,13 @@
 package me.zodd.strax.core.commands
 
 import org.spongepowered.api.command.parameter.CommandContext
-import org.spongepowered.api.command.parameter.CommonParameters
 import org.spongepowered.api.command.parameter.managed.Flag
 import org.spongepowered.api.service.permission.Subject
 
 class CommonCommandFlags internal constructor(private val module: String) {
     val force: Flag = Flag.builder().aliases("force", "f").setPermission("strax.$module.force").build()
+
+    val overwrite: Flag = Flag.builder().aliases("overwrite", "o").setPermission("strax.#$module.overwrite").build()
 
     fun isExempt(ctx: CommandContext, subject: Subject): Boolean {
         return if (!ctx.hasFlag(force)) {
